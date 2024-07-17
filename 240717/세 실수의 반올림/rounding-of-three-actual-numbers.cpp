f18c82cfbfeb46050bbb310da818f37acd027c22
ChanBin Kim <cong8685@naver.com>
2024-07-17 09:15:15 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a, b, c;
+    cin >> a >> b >> c;
+    cout.precision(3);
+    cout << fixed << a << "\n" << b << "\n" << c;
+    return 0;
+}
