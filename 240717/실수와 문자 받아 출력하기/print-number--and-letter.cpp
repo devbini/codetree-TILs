1c3cf99f671e9eb0eb8c2338550fe16565d9519a
ChanBin Kim <cong8685@naver.com>
2024-07-17 09:18:13 +0900

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a;
+    double b,c;
+    cin >> a >> b >> c;
+    cout.precision(2);
+    cout << fixed << a << "\n" << b << "\n" << c;
+    return 0;
+}
