9901014072cd070bb11925cbafec66609011cb09
ChanBin Kim <cong8685@naver.com>
2024-07-18 17:05:07 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240718/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void F_Sol(int N) {
+    for (int i = 0; i < N; i++) cout << "12345^&*()_\n";
+}
+
+int main() {
+    int n;
+    cin >> n;
+    F_Sol(n);
+    return 0;
+}
