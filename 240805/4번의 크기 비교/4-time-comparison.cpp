e1c176eb6c68901af05b9901c259a30a33f0fb3e
ChanBeen Kim <cong8685@naver.com>
2024-08-05 12:46:39 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240805/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c,d,e;
+
+    cin >> a >> b >> c >> d >> e;
+    a > b ? cout << "1\n" : cout << "0\n";
+    a > c ? cout << "1\n" : cout << "0\n";
+    a > d ? cout << "1\n" : cout << "0\n";
+    a > e ? cout << "1\n" : cout << "0\n";
+
+    return 0;
+}
