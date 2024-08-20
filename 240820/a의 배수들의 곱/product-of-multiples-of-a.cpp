4bb245abed7f05182c7dc970288d59099d77bb49
ChanBeen Kim <cong8685@naver.com>
2024-08-20 15:25:07 +0900

    Title: a의 배수들의 곱, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240820/a의 배수들의 곱/product-of-multiples-of-a.cpp
+++ 240820/a의 배수들의 곱/product-of-multiples-of-a.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int x,y, temp = 1;
     cin >> x >> y;
-    for (int i = 1; i < y; i++)
+    for (int i = 1; i <= y; i++)
     {
         if (i % x == 0) temp *= i;
     }
